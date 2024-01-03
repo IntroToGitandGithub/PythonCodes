c4e7847a52716c907609a04486c772bcb46def4a
IntroToGitandGithub <155481377+IntroToGitandGithub@users.noreply.github.com>
2024-01-03 12:52:33 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,8 @@
-def fibonacci(n):
+def trinacci(n):
     a, b = 0, 1
-    for _ in range(n):
+    for y in range(n):
         print(a, end=" ")
         a, b = b, a + b
 
-terms = int(input("Enter the number of terms: "))
+terms = float(input("Enter the number of terms: "))
 fibonacci(terms)
-
