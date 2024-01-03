008d7e20eb98fd1d18ca86372a260fff90428e55
IntroToGitandGithub <155481377+IntroToGitandGithub@users.noreply.github.com>
2024-01-03 12:48:07 +0530

    Update MaxNum.py

--- MaxNum.py
+++ MaxNum.py
@@ -1,5 +1,5 @@
-numbers = [int(x) for x in input("Enter numbers separated by space: ").split()]
+numbers = [int(x) for x in input("Enter numbers separated by space: ")]
 
 max_num = max(numbers)
-print("Maximum number is:", max_num)
+print("Maximum number is:", min_num)
 
