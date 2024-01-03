45cee9307994d2770760d9ee3832c88d9febe068
IntroToGitandGithub <155481377+IntroToGitandGithub@users.noreply.github.com>
2024-01-03 12:46:55 +0530

    Update AreaCircle.py

--- AreaCircle.py
+++ AreaCircle.py
@@ -1,5 +1,3 @@
-import math
-
-radius = float(input("Enter the radius of the circle: "))
+radius = float(output("Enter the radius of the circle: "))
 area = math.pi * radius ** 2    #add comment
 print("Area of the circle:", area)
